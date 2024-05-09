e16377f47d1d9ebb9e3a52ea22377ba190ed3329
Laura-Lepage <lauralepage02@hotmail.com>
2024-05-09 19:15:55 +0200

    début header

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,31 @@ burgerMenu.addEventListener('click', function(){
   menuDown.classList.toggle('menuDownActive')
 
 })
+
+//Menu disparait au Scroll
+let navBox = document.querySelector('.navBox')
+let lastScrollay = 0
+
+function handleScroll(){
+  let dejaScrollay = Math.max(window.scrollY, 0)
+  if (dejaScrollay > lastScrollay){
+    navBox.classList.add('slideMenu')
+  } else {
+    navBox.classList.remove('slideMenu')
+  }
+  lastScrollay = dejaScrollay
+}
+
+window.addEventListener('scroll', handleScroll);
+
+//Apparition des slug HomePage
+
+window.addEventListener('load', function() {
+  const slugs = document.querySelectorAll('.slug');
+  
+  slugs.forEach((slug, index) => {
+    setTimeout(() => {
+      slug.classList.add('slugShow');
+    }, index * 1000 + 500);
+  });
+});
