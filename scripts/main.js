6a05c6f2cebf21bf59554df6574db6466408aa18
Laura-Lepage <lauralepage02@hotmail.com>
2024-05-14 15:58:51 +0200

    fin de journée - 14 mai

--- scripts/main.js
+++ scripts/main.js
@@ -24,16 +24,47 @@ function handleScroll(){
   lastScrollay = dejaScrollay
 }
 
-window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', handleScroll)
 
 //Apparition des slug HomePage
 
 window.addEventListener('load', function() {
-  const slugs = document.querySelectorAll('.slug');
+  const slugs = document.querySelectorAll('.slug')
   
   slugs.forEach((slug, index) => {
     setTimeout(() => {
-      slug.classList.add('slugShow');
-    }, index * 1000 + 500);
-  });
-});
+      slug.classList.add('slugShow')
+    }, index * 1000 + 500)
+  })
+})
+
+//bulle aubergine qui bouge verticalement
+window.addEventListener('scroll', function() {
+  const aubergineCircle = document.querySelector('.aubergineCircle')
+  const scrollY = window.scrollY
+
+  // Calculer la nouvelle position verticale en fonction du scroll
+  const newPosition = `${50 + (scrollY * 0.05)}%` // Ajuste le coefficient selon la vitesse souhaitée
+
+  // Modifier la position verticale de la bulle
+  aubergineCircle.style.top = newPosition
+})
+
+let elements = document.querySelectorAll(".fadeIn")
+// easyscroll
+function easyscroll(){
+  elements.forEach((element, index) => {
+      if((window.scrollY + window.innerHeight) > (element.offsetTop +(element.offsetHeight/2))){
+        setTimeout(() => {
+          element.classList.add("visible")
+        }, index * 500)
+       
+       }     
+   })
+}
+
+easyscroll()
+window.addEventListener ("scroll", function (){
+  easyscroll()
+ 
+  })
